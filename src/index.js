a6bfddc7d1d9aeb73b421d19e004b90f11b50dc9
Karl Juri Ots <kjots@kjots.org>
2016-06-16 17:22:13 +1000

    Added expressDataRouter().

--- src/index.js
+++ src/index.js
@@ -1,1 +1,82 @@
-export default {};
+import _ from 'lodash';
+import bodyParser from 'body-parser';
+import express from 'express';
+import fastJsonPatch from 'fast-json-patch';
+
+export default function expressDataRouter({ data = {}, limit } = {}) {
+    function getData(path) {
+        // See https://github.com/Starcounter-Jack/JSON-Patch/issues/66
+        let pointer = { op: '_get', path };
+
+        fastJsonPatch.apply(data, [ pointer ]);
+
+        return pointer.value;
+    }
+    
+    function clearData() {
+        Object.keys(data).forEach(key => delete data[key]);
+    }
+
+    return express.Router()
+        .get('/', (req, res) => {
+            return res.json(data);
+        })
+        .get('/*', (req, res) => {
+            let value = getData(req.path);
+            if (value === undefined) {
+                return res.status(404).end();
+            }
+
+            return res.json(value);
+        })
+        .delete('/', (req, res) => {
+            clearData();
+
+            return res.end();
+        })
+        .delete('/*', (req, res) => {
+            fastJsonPatch.apply(data, [{ op: 'remove', path: req.path }]);
+
+            return res.end();
+        })
+        .use(bodyParser.json({ limit }))
+        .put('/', (req, res) => {
+            clearData();
+
+            _.merge(data, req.body);
+
+            return res.status(201).json(data);
+        })
+        .put('/*', (req, res) => {
+            fastJsonPatch.apply(data, [{ op: 'replace', path: req.path, value: req.body }]);
+
+            return res.status(201).json(req.body);
+        })
+        .post('/', (req, res) => {
+            _.merge(data, req.body);
+
+            return res.status(201).json(data);
+        })
+        .post('/*', (req, res) => {
+            let value = _.merge({}, getData(req.path), req.body);
+
+            fastJsonPatch.apply(data, [{ op: 'replace', path: req.path, value }]);
+
+            return res.status(201).json(value);
+        })
+        .patch('/', (req, res) => {
+            fastJsonPatch.apply(data, Array.isArray(req.body) ? req.body : [ req.body ]);
+
+            return res.status(201).json(data);
+        })
+        .patch('/*', (req, res) => {
+            let value = getData(req.path);
+            if (value === undefined) {
+                return res.status(404).end();
+            }
+
+            fastJsonPatch.apply(value, Array.isArray(req.body) ? req.body : [ req.body ]);
+
+            return res.status(201).json(value);
+        });
+}
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,276 @@
-import index from './index.js';
+import nodeMocksHttp from 'node-mocks-http';
+import sinon from 'sinon';
 
-describe('index', () => {
-    it('should exist', () => {
-        expect(index).to.not.be.undefined;
+import expressJsonData from './index.js';
+
+describe('expressJsonData()', () => {
+    let data;
+
+    beforeEach(() => {
+        data = { testItem: { testItemKey: 'Test Item Value' } };
+    });
+
+    context('on GET /', () => {
+        it('should respond with the data', () => {
+            // Given
+            let req = nodeMocksHttp.createRequest({ method: 'GET', url: '/' });
+            let res = nodeMocksHttp.createResponse();
+
+            sinon.spy(res, 'json');
+
+            // When
+            expressJsonData({ data })(req, res);
+
+            // Then
+            expect(res.json).to.have.been.calledWith({ testItem: { testItemKey: 'Test Item Value' } });
+        });
+    });
+
+    context('on GET /*', () => {
+        context('when the data item exists', () => {
+            it('should respond with the data item', () => {
+                // Given
+                let req = nodeMocksHttp.createRequest({ method: 'GET', url: '/testItem' });
+                let res = nodeMocksHttp.createResponse();
+
+                sinon.spy(res, 'json');
+
+                // When
+                expressJsonData({ data })(req, res);
+
+                // Then
+                expect(res.json).to.have.been.calledWith({ testItemKey: 'Test Item Value' });
+            });
+        });
+
+        context('when the data item does not exist', () => {
+            it('should respond with HTTP 404 Not Found', () => {
+                // Given
+                let req = nodeMocksHttp.createRequest({ method: 'GET', url: '/notTestItem' });
+                let res = nodeMocksHttp.createResponse();
+
+                sinon.spy(res, 'status');
+                sinon.spy(res, 'end');
+
+                // When
+                expressJsonData({ data })(req, res);
+
+                // Then
+                expect(res.status).to.have.been.calledWith(404);
+                expect(res.end).to.have.been.called;
+            });
+        });
+    });
+
+    context('on DELETE /', () => {
+        it('should remove the data', () => {
+            // Given
+            let req = nodeMocksHttp.createRequest({ method: 'DELETE', url: '/' });
+            let res = nodeMocksHttp.createResponse();
+
+            // When
+            expressJsonData({ data })(req, res);
+
+            // Then
+            expect(data).to.eql({});
+        });
+    });
+
+    context('on DELETE /*', () => {
+        it('should remove the data item', () => {
+            // Given
+            let req = nodeMocksHttp.createRequest({ method: 'DELETE', url: '/testItem' });
+            let res = nodeMocksHttp.createResponse();
+
+            // When
+            expressJsonData({ data })(req, res);
+
+            // Then
+            expect(data).to.eql({});
+        });
+    });
+
+    context('on PUT /', () => {
+        it('should replace the data with the request data', () => {
+            // Given
+            let req = nodeMocksHttp.createRequest({ method: 'PUT', url: '/', body: { newTestItem: { newTestItemKey: 'New Test Item Value' } } });
+            let res = nodeMocksHttp.createResponse();
+
+            sinon.spy(res, 'status');
+            sinon.spy(res, 'json');
+
+            // When
+            expressJsonData({ data })(req, res);
+
+            // Then
+            expect(data).to.eql({ newTestItem: { newTestItemKey: 'New Test Item Value' } });
+
+            expect(res.status).to.have.been.calledWith(201);
+            expect(res.json).to.have.been.calledWith({ newTestItem: { newTestItemKey: 'New Test Item Value' } });
+        });
+    });
+
+    context('on PUT /*', () => {
+        it('should replace the data item with the request data', () => {
+            // Given
+            let req = nodeMocksHttp.createRequest({ method: 'PUT', url: '/testItem', body: { newTestItemKey: 'New Test Item Value' } });
+            let res = nodeMocksHttp.createResponse();
+
+            sinon.spy(res, 'status');
+            sinon.spy(res, 'json');
+
+            // When
+            expressJsonData({ data })(req, res);
+
+            // Then
+            expect(data).to.eql({ testItem: { newTestItemKey: 'New Test Item Value' } });
+
+            expect(res.status).to.have.been.calledWith(201);
+            expect(res.json).to.have.been.calledWith({ newTestItemKey: 'New Test Item Value' });
+        });
+    });
+
+    context('on POST /', () => {
+        it('should merge the request data into the data', () => {
+            // Given
+            let req = nodeMocksHttp.createRequest({ method: 'POST', url: '/', body: { newTestItem: { newTestItemKey: 'New Test Item Value' } } });
+            let res = nodeMocksHttp.createResponse();
+
+            sinon.spy(res, 'status');
+            sinon.spy(res, 'json');
+
+            // When
+            expressJsonData({ data })(req, res);
+
+            // Then
+            expect(data).to.eql({ testItem: { testItemKey: 'Test Item Value' }, newTestItem: { newTestItemKey: 'New Test Item Value' } });
+
+            expect(res.status).to.have.been.calledWith(201);
+            expect(res.json).to.have.been.calledWith({ testItem: { testItemKey: 'Test Item Value' }, newTestItem: { newTestItemKey: 'New Test Item Value' } });
+        });
+    });
+
+    context('on POST /*', () => {
+        it('should merge the request data into the data item', () => {
+            // Given
+            let req = nodeMocksHttp.createRequest({ method: 'POST', url: '/testItem', body: { newTestItemKey: 'New Test Item Value' } });
+            let res = nodeMocksHttp.createResponse();
+
+            sinon.spy(res, 'status');
+            sinon.spy(res, 'json');
+
+            // When
+            expressJsonData({ data })(req, res);
+
+            // Then
+            expect(data).to.eql({ testItem: { testItemKey: 'Test Item Value', newTestItemKey: 'New Test Item Value' } });
+
+            expect(res.status).to.have.been.calledWith(201);
+            expect(res.json).to.have.been.calledWith({ testItemKey: 'Test Item Value', newTestItemKey: 'New Test Item Value' });
+        });
+    });
+    
+    context('on PATCH /', () => {
+        context('when the request body is an object', () => {
+            it('should apply the request body to the data as a JSON Patch', () => {
+                // Given
+                let req = nodeMocksHttp.createRequest({ method: 'PATCH', url: '/', body: { op: 'replace', path: '/testItem', value: { newTestItemKey: 'New Test Item Value' } } });
+                let res = nodeMocksHttp.createResponse();
+
+                sinon.spy(res, 'status');
+                sinon.spy(res, 'json');
+
+                // When
+                expressJsonData({ data })(req, res);
+
+                // Then
+                expect(data).to.eql({ testItem: { newTestItemKey: 'New Test Item Value' } });
+
+                expect(res.status).to.have.been.calledWith(201);
+                expect(res.json).to.have.been.calledWith({ testItem: { newTestItemKey: 'New Test Item Value' } });
+            });
+        });
+
+        context('when the request body is an array', () => {
+            it('should apply the request body to the data as a JSON Patch', () => {
+                // Given
+                let req = nodeMocksHttp.createRequest({ method: 'PATCH', url: '/', body: [{ op: 'replace', path: '/testItem', value: { newTestItemKey: 'New Test Item Value' } }] });
+                let res = nodeMocksHttp.createResponse();
+
+                sinon.spy(res, 'status');
+                sinon.spy(res, 'json');
+
+                // When
+                expressJsonData({ data })(req, res);
+
+                // Then
+                expect(data).to.eql({ testItem: { newTestItemKey: 'New Test Item Value' } });
+
+                expect(res.status).to.have.been.calledWith(201);
+                expect(res.json).to.have.been.calledWith({ testItem: { newTestItemKey: 'New Test Item Value' } });
+            });
+        });
+    });
+
+    context('on PATCH /*', () => {
+        context('when the data item exists', () => {
+            context('when the request body is an object', () => {
+                it('should apply the request body to the data item as a JSON Patch', () => {
+                    // Given
+                    let req = nodeMocksHttp.createRequest({ method: 'PATCH', url: '/testItem', body: { op: 'replace', path: '/testItemKey', value: 'New Test Item Value' } });
+                    let res = nodeMocksHttp.createResponse();
+
+                    sinon.spy(res, 'status');
+                    sinon.spy(res, 'json');
+
+                    // When
+                    expressJsonData({ data })(req, res);
+
+                    // Then
+                    expect(data).to.eql({ testItem: { testItemKey: 'New Test Item Value' } });
+
+                    expect(res.status).to.have.been.calledWith(201);
+                    expect(res.json).to.have.been.calledWith({ testItemKey: 'New Test Item Value' });
+                });
+            });
+
+            context('when the request body is an array', () => {
+                it('should apply the request body to the data item as a JSON Patch', () => {
+                    // Given
+                    let req = nodeMocksHttp.createRequest({ method: 'PATCH', url: '/testItem', body: [{ op: 'replace', path: '/testItemKey', value: 'New Test Item Value' }] });
+                    let res = nodeMocksHttp.createResponse();
+
+                    sinon.spy(res, 'status');
+                    sinon.spy(res, 'json');
+
+                    // When
+                    expressJsonData({ data })(req, res);
+
+                    // Then
+                    expect(data).to.eql({ testItem: { testItemKey: 'New Test Item Value' } });
+
+                    expect(res.status).to.have.been.calledWith(201);
+                    expect(res.json).to.have.been.calledWith({ testItemKey: 'New Test Item Value' });
+                });
+            });
+        });
+
+        context('when the data item does not exist', () => {
+            it('should respond with HTTP 404 Not Found', () => {
+                // Given
+                let req = nodeMocksHttp.createRequest({ method: 'PATCH', url: '/notTestItem' });
+                let res = nodeMocksHttp.createResponse();
+
+                sinon.spy(res, 'status');
+                sinon.spy(res, 'end');
+
+                // When
+                expressJsonData({ data })(req, res);
+
+                // Then
+                expect(res.status).to.have.been.calledWith(404);
+                expect(res.end).to.have.been.called;
+            });
+        });
     });
 });
