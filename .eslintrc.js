60ba2507a07ec24fe1c2895d0a39e0645f2588bb
Karl Juri Ots <kjots@kjots.org>
2017-01-15 23:07:40 +1100

    Updated ESLint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
-        es6: true
+        es6: true,
+        node: true
     },
     extends: 'eslint:recommended',
     parserOptions: {
@@ -8,10 +9,19 @@ module.exports = {
         sourceType: 'module'
     },
     rules: {
-        'strict': [ 'error', 'global' ],
-        'indent': [ 'error', 4 ],
+        'brace-style': [ 2, 'stroustrup', { allowSingleLine: true } ],
+        'comma-dangle': [ 'error', 'never' ],
+        'eqeqeq': [ 'error', 'always' ],
+        'indent': [ 'error', 4, { SwitchCase: 1 } ],
+        'keyword-spacing': [ 'error', { before: true, after: true } ],
         'linebreak-style': [ 'error', 'unix' ],
+        'no-cond-assign': [ 'error', 'always' ],
+        'no-unneeded-ternary': [ 'error', { defaultAssignment: false } ],
+        'no-var': 'error',
         'quotes': [ 'error', 'single' ],
-        'semi': [ 'error', 'always' ]
+        'semi': [ 'error', 'always' ],
+        'space-before-blocks': [ 'error', 'always' ],
+        'strict': [ 'error', 'global' ],
+        'yoda': [ 'error', 'never' ]
     }
 };
